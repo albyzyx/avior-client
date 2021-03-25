675a6553ddc2478bf455722df35ce58b4f264c33
Albin <albinarun24@gmail.com>
2021-03-25 13:31:34 +0530

    Sign in form 02.11

--- lib/application/auth/sign_up_form/sign_up_form_bloc.dart
+++ lib/application/auth/sign_up_form/sign_up_form_bloc.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
+import 'package:avior/domain/auth/value_objects.dart';
+import 'package:avior/domain/core/auth_failure.dart';
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'sign_up_form_event.dart';
